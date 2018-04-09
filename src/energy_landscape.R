5a2f8aed6ce7002334ce6a848403596671492b01
Max Czapanskiy <maxczapanskiy@gmail.com>
2018-04-09 15:10:33 -0400

    energy landscape: create connections, annotate with wind values, predict energy costs, calculate round-trip costs. next up: interpolate surface.

--- src/energy_landscape.R
+++ src/energy_landscape.R
@@ -1,10 +1,8 @@
 library(tidyverse)
 library(sp)
+library(igraph)
 
-energy_landscape <- function(mvmt_mod, wind_uv, col_loc, for_rng, grd_sz) {
-  
-}
-
+# Create hexagonal grid 
 create_grid <- function(origin, radius, spacing) {
   if(length(origin) != 2 || class(origin) != 'numeric')
     stop('origin must be x,y coordinates (numeric vector of length 2)')
@@ -36,16 +34,21 @@ create_grid <- function(origin, radius, spacing) {
          y = y - o_error$dy,
          d2o = sqrt((x - o[1])^2 + (y - o[2])^2)) %>%
     filter(d2o <= radius) %>%
-    select(i, j, x, y)
+    mutate(id = row_number()) %>%
+    select(id, i, j, x, y)
 }
 
-grid_coords <- create_grid(c(30, 30), 30, 15)
+origin <- c(30, 30)
+radius <- 30
+spacing <- 15
+grid_coords <- create_grid(origin, radius, spacing)
 ggplot(grid_coords, aes(x, y)) + 
   geom_text(aes(label = sprintf('(%i, %i)', i, j))) + 
-  annotate('point', 30, 30, color = 'red') + 
+  annotate('point', origin[1], origin[2], color = 'red') + 
   geom_path(data = circ_coords, linetype = 'dashed') + 
   coord_fixed()
 
+# Connect neighboring grid points
 connect_neighbors <- function(grid_coords) {
   neighbors <- function(i, j) {
     # left-to-right, top-to-bottom
@@ -64,22 +67,125 @@ connect_neighbors <- function(grid_coords) {
                j2 = j + y_6)
   }
   
-  grid_coords2 <- rename(grid_coords, x2 = x, y2 = y)
+  grid_coords2 <- rename(grid_coords, id2 = id, x2 = x, y2 = y)
   
   grid_coords %>%
     rowwise %>%
     do(neighbors(.$i, .$j)) %>%
     right_join(grid_coords, by = c('i' = 'i', 'j' = 'j')) %>%
-    right_join(grid_coords2, by = c('i2' = 'i', 'j2' = 'j'))
+    right_join(grid_coords2, by = c('i2' = 'i', 'j2' = 'j')) %>%
+    mutate(azimuth_out = atan2(y2 - y, x2 - x),
+           azimuth_in = pi - azimuth_out,
+           distance = sqrt((x2 - x)^2 + (y2 - y)^2))
 }
 
-neighbors <- connect_neighbors(grid_coords)
+connections <- connect_neighbors(grid_coords)
 ggplot(grid_coords, aes(x, y)) + 
   geom_text(aes(label = sprintf('(%i, %i)', i, j))) + 
   annotate('point', 30, 30, color = 'red') + 
   geom_path(data = circ_coords, linetype = 'dashed') +
   geom_segment(aes(x, y, xend = x2, yend = y2), 
-               data = neighbors,
+               data = connections,
                arrow = arrow(length = unit(0.03, 'npc'))) +
   coord_fixed()
 
+# dummy wind
+wind_u <- matrix(seq(from = -5, to = 10, length.out = 100), nrow = 10) %>%
+  raster(xmn = 0, xmx = 60, ymn = 0, ymx = 60)
+wind_v <- matrix(seq(from = 5, to = -10, length.out = 100), nrow = 10) %>%
+  raster(xmn = 0, xmx = 60, ymn = 0, ymx = 60)
+
+load('data/out/Models/EnergyModels.Rdata')
+load('data/out/Models/SgTCModel.RData')
+
+pred_spd <- function(t) {
+  predict(SgTCModel, 
+          re.form = NA,
+          newdata = data.frame(Tailwind = t))
+}
+pred_energy <- function(a, m) {
+  predict(fam, 
+          newdata = data.frame(WindAngle = a,
+                               WindSpd = m,
+                               DeployID = 1145),
+          exclude = 's(DeployID)',
+          type = 'response')
+}
+
+# Annotate connections with wind conditions and trip predictions
+annotate_wind <- function(connections, u, v, pred_spd, pred_energy) {
+  sample_raster <- function(x1, y1, x2, y2, r) {
+    len <- sqrt((x2 - x1)^2 + (y2 - y1)^2)
+    npoints <- len / xres(r) + 1
+    points <- approx(c(x1, x2), c(y1, y2), n = npoints)
+    cbind(points$x, points$y) %>%
+      cellFromXY(r, .) %>%
+      raster::extract(r, .) %>%
+      mean
+  }
+  
+  vector_angle <- function(u, v) {
+    dot <- function(u, v) sum(u * v)
+    norm <- function(u) sqrt(sum(u^2))
+    acos(dot(u, v) / (norm(u) * norm(v))) %>%
+      round(digits = 3)
+  }
+  
+  mutate(connections,
+         mean_u = mapply(FUN = sample_raster, 
+                         x, y, x2, y2, 
+                         MoreArgs = list(r = wind_u)),
+         mean_v = mapply(FUN = sample_raster, 
+                         x, y, x2, y2, 
+                         MoreArgs = list(r = wind_v)),
+         wind_angle_out = vector_angle(c(x2 - x, y2 - y), c(mean_u, mean_v)),
+         wind_angle_in = vector_angle(c(x - x2, y - y2), c(mean_u, mean_v)),
+         wind_speed = sqrt(mean_u^2 + mean_v^2),
+         tailwind_out = wind_speed * cos(wind_angle_out),
+         tailwind_in = wind_speed * cos(wind_angle_in),
+         flight_speed_out = pred_spd(tailwind_out),
+         duration_out = flight_speed_out / distance,
+         flap_ratio_out = pred_energy(wind_angle_out, wind_speed),
+         flap_out = duration_out * flap_ratio_out / 60,
+         flight_speed_in = pred_spd(tailwind_in),
+         duration_in = flight_speed_in / distance,
+         flap_ratio_in = pred_energy(wind_angle_in, wind_speed),
+         flap_in = duration_in * flap_ratio_in / 60)
+}
+
+wind_conns <- annotate_wind(connections, wind_u, wind_v, pred_spd, pred_energy)
+
+# Calculate trip costs
+## NOTE: uses duration as weight. This is hard-coded and needs future editing.
+trip_costs <- function(grid, conns, origin) {
+  origin_id <- filter(grid, x == origin[1], y == origin[2])$id
+  
+  out_mat <- transmute(conns, id, id2,
+                       weight = duration_out) %>%
+    graph_from_data_frame(directed = TRUE,
+                          vertices = grid) %>%
+    distances(graph = .,
+              mode = 'out', 
+              weights = NULL, 
+              algorithm = 'dijkstra')
+  out_cost <- out_mat[origin_id, ]
+  
+  in_mat <- transmute(conns, id, id2,
+                      weight = duration_in) %>%
+    graph_from_data_frame(directed = TRUE,
+                          vertices = grid_coords) %>%
+    distances(graph = .,
+              mode = 'in', 
+              weights = NULL, 
+              algorithm = 'dijkstra')
+  in_cost <- in_mat[origin_id, ]
+  
+  mutate(grid,
+         out_cost = out_cost,
+         in_cost = in_cost,
+         rt_cost = out_cost + in_cost)
+}
+
+grid_costs <- trip_costs(grid_coords, wind_conns, origin)
+
+## TODO: Interpolate surface
